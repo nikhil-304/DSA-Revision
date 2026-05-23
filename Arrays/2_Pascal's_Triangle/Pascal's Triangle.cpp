77ee4a7bd5b53329e2654c963f96d816d209f442
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2026-05-23 14:50:55 +0530

    DSA Revision "Pascal's Triangle I" problem to generate 'n' rows dated: 23/05/2026

--- Arrays/2_Pascal's_Triangle/Pascal's Triangle.cpp
+++ Arrays/2_Pascal's_Triangle/Pascal's Triangle.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     vector<int> generateRows(int nRow){
+        // eg: 4th Row
         vector<int> pascalAns;
         long long ans = 1;
+        // in 4th row, push the 0th index value
+        // eg: [1, ...] 
+        //    0th element
         pascalAns.push_back(ans);
         
+        // loop for populating values after 0th index
+        // eg: [1, 3, 3, 1]
+           // 0th, 1st, 2nd, 3rd
         for(int i=1 ; i<nRow ; i++){
+            // ans = ans * (nRow - i) / i;
             ans = ans * (nRow-i);
             ans = ans / i;
             pascalAns.push_back(ans);
