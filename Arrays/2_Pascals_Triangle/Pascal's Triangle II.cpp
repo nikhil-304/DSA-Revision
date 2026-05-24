e34907cbbc2bfbc9dd607c4fd1ea11af901ae88c
Nikhil Shrivastava <nikhil.shrivastava304@gmail.com>
2026-05-24 12:52:38 +0530

    DSA Revision: Update "Pascal's Triangle II" to generate 'nth' row dated: 24/05/2026

--- Arrays/2_Pascals_Triangle/Pascal's Triangle II.cpp
+++ Arrays/2_Pascals_Triangle/Pascal's Triangle II.cpp
@@ -1,13 +1,23 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
+        // here rowIndex + 1 because the input is foll. 0th index analogy, which means, 4th row will have it's input here as rowIndex = 3, but we'll follow the rowNum analogy
+        int rowNum = rowIndex + 1;
+
+        // eg: 4th Row
         vector<int> finalAns;
         long long ans = 1;
+        // in 4th row, push the 0th index value
+        // eg: [1, ...]
+        //    0th element
         finalAns.push_back(ans);
 
-        for(int i=0 ; i<rowIndex ; i++){
-            ans = ans * (rowIndex - i);
-            ans = ans / (i+1);
+        // loop for populating values after 0th index
+        // eg: [1, 3, 3, 1]
+           // 0th, 1st, 2nd, 3rd
+        for(int i=1 ; i<rowNum ; i++){
+            ans = ans * (rowNum - i);
+            ans = ans / (i);
             finalAns.push_back(ans);
         }
         return finalAns;
@@ -22,3 +32,4 @@ Space Complexity: O(rowIndex)
 //Problem Link: https://leetcode.com/problems/pascals-triangle-ii/
 
 //Revision done on: 2/2/2026
+//Revision done on: 14/05/2026
